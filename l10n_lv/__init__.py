1490b2f4cadb9d8c30d502a520a38c6b2607c384
Arnis Putnins <arnis@allegro.lv>
2022-12-14 12:59:01 +0200

    a brand new version (in collaboration with Chick.Farm)

--- l10n_lv/__init__.py
+++ l10n_lv/__init__.py
@@ -1,25 +1,8 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Part of Odoo.
-#    Copyright (C) 2021 Allegro IT (<http://www.allegro.lv/>)
-#                       E-mail: <info@allegro.lv>
-#                       Address: <Vienibas gatve 109 LV-1058 Riga Latvia>
-#                       Phone: +371 67289467
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+from odoo import api, SUPERUSER_ID
 
-from . import models
+
+def load_translations(cr, registry):
+    """Load template translations."""
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env.ref(
+        'l10n_lv.account_chart_template_latvia').process_coa_translations()
--- l10n_lv/__manifest__.py
+++ l10n_lv/__manifest__.py
@@ -1,73 +1,45 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Part of Odoo.
-#    Copyright (C) 2022 Allegro IT (<https://allegro.lv/>)
-#                       E-mail: <info@allegro.lv>
-#                       Address: <Vienibas gatve 109 LV-1058 Riga Latvia>
-#                       Phone: +371 67289467
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 {
-    "name": "Latvia - Accounting",
-    "version": "1.1",
-    "category": "Localization",
-    "summary": "Accounting configuration for Latvia",
-    "description": """
-Adds Chart of accounts, Chart of taxes, Fiscal positions, Banks and Partner titles.
-""",
-    "author": "Allegro IT",
-    "website": "https://allegro.lv",
-    "license": "LGPL-3",
-    "images": [],
-    "depends": [
-        "account"
-        "l10n_multilang",
+    'name': "Latvia - Accounting",
+    'version': '16.0.0.5',
+    'description': """
+        Chart of Accounts (COA) Template for Latvia's Accounting.
+
+        This module also includes:
+
+        * Tax groups.
+        * Most common Latvian Taxes.
+        * Fiscal positions.
+        * Account Tags.
+        * Latvian and Lithuanian banks list.
+        co-author is Chick.Farm (visit for more information https://www.myacc.cloud)
+    """,
+    'license': 'LGPL-3',
+    'author': "Allegto IT, Chick.Farm",
+    'website': "https://allegro.lv",
+    'category': 'Accounting/Localizations/Account Charts',
+    'depends': [
     ],
-    "data": [
+    'data': [
+# Data for Trial balance and profit/loss report
+        'data/account_account_tag_data.xml',
 
-        # Chart of Accounts
-        "data/account_chart_template_data.xml",
-        "data/account_account_tag_data.xml",
-        "data/account.account.template.csv",
-        "data/account.group.template.csv",
-
-        #      "data/account_account_type_data.xml",
-        #      "data/account_group_template_data.xml",
-        #      "data/account_account_template_data.xml",
-
-        # Taxes
-        "data/account_tax_group_data.xml",
-        "data/account_tax_report_data.xml",
-        "data/account_tax_template_data.xml",
-        "data/account_fiscal_position_template_data.xml",
-        "data/account_account_template_post_data.xml",
-
-        # Views and others
-        "views/res_partner_view.xml"
-        "data/update_data.xml",
-        "data/res_company_data.xml",
-        "data/res_partner_title_data.xml",
-        "data/res.bank.csv",
-        "data/account_payment_terms_data.xml",
-
+# Information about accounting plan
+        'data/account_chart_template_data.xml',
+# Accounting plan
+        'data/account.account.template.csv',
+        'data/account.group.template.csv',
+#Jornal data
+        'data/account_chart_template_setup_data.xml',
+        'data/res_bank_data_lv.xml',
+        'data/res_bank_data_lt.xml',
+        'data/account_tax_group_data.xml',
+        'data/account_tax_template_data.xml',
+        'data/account_fiscal_position_template_data.xml',
+# Try Loading COA for Current Company
+        'data/account_chart_template_load.xml',
     ],
-    "demo": [],
-    "installable": True,
-    "auto_install": False,
-    "qweb": [],
+
+    'post_init_hook': 'load_translations',
+    'installable': True,
 }
